d3272839a6acf972ecf8a6958c3a85245df01015
Chad Laing <chadlaing@gmail.com>
2013-01-30 16:09:46 -0700

    removing Test Directory from master

--- Test/Examples/object_example.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-
-use strict;
-use warnings;
-use Log::Log4perl qw(:easy); #easy provides a beginners interface to learn the system
-
-#In Log4perl logger obhects do the work and they are obtained by calling get_logger();
-#This effectively returns a reference to a logger, and this can be called by all the functions
