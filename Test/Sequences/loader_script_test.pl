b9659b5104d1eab857d76207f23ccfb8b1bf009d
amanji <amanji@stec.phac.gc.ca>
2013-02-12 09:20:59 -0700

    Fasta header appender now works but still needs polishing

--- Test/Sequences/loader_script_test.pl
+++ Test/Sequences/loader_script_test.pl
@@ -4,41 +4,40 @@ use strict;
 use warnings;
 use IO::File;
 
-my $attributes;
+#NOTE: This code works, but is still buggy and needs to be tightened up and tested thoroughly before using in production.
+#	See the accompanying readme for instructions on how to use this script.
+
 my $filename = $ARGV[0];
+my $attributes = "";
+main();
 
 sub main {
-_readInAttributes();
+	_readInAttributes();
 ####Do not modify anything past this line####
-#my $attributes = _addAttributes(%attributes);
-my @args = ("gmod_fasta2gff3.pl" . " --attributes " . $attributes);
+my @args = ("gmod_fasta2gff3.pl" . " --attributes " . '"'.$attributes.'"');
 system(@args) == 0 or die "System with @args failed: $? \n";
-printf "System executed @args with value %d\n" , $? >> 8;
+printf "System executed @args with value %d\n", $? >> 8;
 }
 
 sub _readInAttributes {
 	my $newFH = IO::File->new('<' . $filename)	or die "$!\n";
 	while(my $line = $newFH->getline) {
+		#_setKeyVal($line);
 		_appendAttributes($line);
 	}
 }
 
-# sub _printAttributes{
-# for (keys %attributes) {
-# 	print "$_ = $attributes{$_}\n"; 
+# sub _setKeyVal {
+# 	my $line = shift;
+# 	if ($line =~ /(^[\w]+)\t(([\w]+;?[^\n]+))/m) {
+# 		#create key val hash pairs
+#		print "$1=$2";
 # 	}
 # }
 
-sub _setKeyVal {
-	my $line = shift;
-	if ($line =~ /(^[\w]+)\t(([\w]+;?[^\n]+))/m) {
-		print "$1=$2\n";
-	}
-}
-
 sub _appendAttributes {
 	my $line = shift;
-	if ($line =~ /(^[\w]+)\t(([\w]+;?[^\n]+))/m) {
-		my $attributes = "$1=$2";
+	if ($line =~ /(^[\w]+)\t(([\w]+;?[^\n]+))/s) {
+		$attributes = "$attributes"."$1=$2";
 	}
 }
