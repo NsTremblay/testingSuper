bc79e079123939c5e540dbe5723931a4350321b1
Akiff Manji <amanji@stec-ThinkCentre-A62.(none)>
2013-05-15 09:55:18 -0600

    Adding site uploader to maser

--- Modules/GenomeUploader.pm
+++ Modules/GenomeUploader.pm
@@ -12,7 +12,7 @@ Modules::GenomeUploader
 
 =head1 ACKNOWLEDGMENTS
 
-Thank you to Dr. Chad Laing and Dr. Michael Whiteside, for all their assistance on this project
+Thank you to Dr. Chad Laing and Dr. Matthew Whiteside, for all their assistance on this project
 
 =head1 COPYRIGHT
 
@@ -35,7 +35,7 @@ package Modules::GenomeUploader;
 use strict;
 use warnings;
 use FindBin;
-use lib 'FindBin::Bin/../';
+use lib "$FindBin::Bin/../";
 use IO::File;
 use IO::Dir;
 use Bio::SeqIO;
@@ -51,26 +51,15 @@ Run modes are passed in as <reference name>=><subroutine name>
 =cut
 
 sub setup {
-	my $self=shift;
-	my $logger = Log::Log4perl->get_logger();
-	$logger->info("Logger initialized in Modules::GenomeUploader");
-	$self->start_mode('default');
-	$self->run_modes(
-		'default'=>'default',
-		'genome_uploader'=>'genomeUploader',
-		'upload_genome'=>'uploadGenome'
-		);
-
-	# $self->connectDatabase({
-	# 	'dbi'=>'Pg',
-	# 	'dbName'=>'chado_db_test',
-	# 	'dbHost'=>'localhost',
-	# 	'dbPort'=>'5432',
-	# 	'dbUser'=>'postgres',
-	# 	'dbPass'=>'postgres'
-	# 	});
-
-	#NOTE: This connects to the dummy database to test uploading genomes
+    my $self=shift;
+    $self->logger(Log::Log4perl->get_logger());
+    $self->logger->info("Logger initialized in Modules::GenomeUploader");
+    $self->start_mode('default');
+    $self->run_modes(
+        'default'=>'default',
+        'genome_uploader'=>'genomeUploader',
+        'upload_genome'=>'uploadGenomeFile'
+        );
 }
 
 =head2 default
@@ -80,9 +69,9 @@ Default start mode. Must be decalared or CGI:Application will die.
 =cut
 
 sub default {
-	my $self = shift;
-	my $template = $self->load_tmpl ( 'hello.tmpl' , die_on_bad_params=>0 );
-	return $template->output();
+    my $self = shift;
+    my $template = $self->load_tmpl ( 'hello.tmpl' , die_on_bad_params=>0 );
+    return $template->output();
 }
 
 =head2 genomeUploader
@@ -92,9 +81,82 @@ Run mode for the genome uploader package
 =cut
 
 sub genomeUploader {
-	my $self = shift;
-	my $template = $self->load_tmpl ( 'genome_uploader.tmpl' , die_on_bad_params=>0 );
-	return $template->output();
+    my $self = shift;
+    my $template = $self->load_tmpl ( 'genome_uploader.tmpl' , die_on_bad_params=>0 );
+    return $template->output();
+}
+
+=head2
+
+Assigns all values to class functions
+
+=cut
+
+sub _initialize {
+    my $self = shift;
+    my $q = $self->query();
+    my %fileTags = $q->Vars;
+    my $genomeFile = 
+    $self->_genomeFile($q->upload("genome_file"));
+    $self->_genomeFileName($fileTags{'genome_file'});
+    $self->_genomeName($fileTags{'genome_of'});
+    $self->_uploadDir("$FindBin::Bin/../../Sequences/uploaderTemp");
+    $self->_formInputs(\%fileTags);
+}
+
+=head2
+
+Stores a genome file for the module
+
+=cut
+
+sub _genomeFile {
+    my $self = shift;
+    $self->{'_genomeFile'} = shift //return $self->{'_genomeFile'};
+}
+
+=head2
+
+Stores a genome file name for the module
+
+=cut
+
+sub _genomeFileName {
+    my $self = shift;
+    $self->{'_genomeFileName'} = shift //return $self->{'_genomeFileName'};
+}
+
+=head2
+
+Stores a genome name for the module
+
+=cut
+
+sub _genomeName {
+    my $self = shift;
+    $self->{'_genomeName'} = shift // return $self->{'_genomeName'};
+}
+
+=head2
+
+Stores an upload directory for the module
+
+=cut
+
+sub _uploadDir {
+    my $self = shift;
+    $self->{'_uploadDir'} = shift // return $self->{'_uploadDir'};
+}
+
+=head2
+
+Stores all inputs passed from the form
+
+=cut
+
+sub _formInputs {
+    my $self = shift;
+    $self->{'_formInputs'} = shift // return $self->{'_formInputs'};
 }
 
 =head2 uploadGenome
@@ -103,86 +165,34 @@ Run mode to upload a user genome.
 
 =cut
 
-sub uploadGenome {
-	my $self = shift;
-	my $q = $self->query();
+sub uploadGenomeFile {
+    my $self = shift;
 
-	my $genomeFileName = $q->param("genomeFile");
-	my $genomeFile = $q->upload("genomeFile");
+    $self->_initialize();
+    $self->config_file("$FindBin::Bin/../../Modules/chado_upload_test.cfg");
 
-	#Create a hash to store the form inputs.
-	my %fileTags;
+    system("mkdir -m 7777 " . $self->_uploadDir) == 0 or die $self->logger->info("System with args failed: $?");
+    my $outHandle = IO::File->new('>' . $self->_uploadDir . "/" . $self->_genomeFileName) or die "$!";
 
-	#Here is where we will query for all documents in the form
-	#The fields guaranteed to be set by the form are:
-	#
-	my $genomeName = $q->param("genomeName");
-	$fileTags{'genome_of'} = $genomeName;
-	my $aliasName = $q->param("aliasName");
-	#alias does not exist in the data base
-	$fileTags{'alias'} = $aliasName;
-	my $isolationDate = $q->param("isolationDate");
-	$fileTags{'date'} = $isolationDate;
-	
-	#source is throwing an error
-	my $hostSource = $q->param("hostSource");
-	$fileTags{'host_source'} = $hostSource;
-	my $isolationLocation = $q->param("isolationLocation");
-	#location does not exist in the databse
-	$fileTags{'location'} = $isolationLocation;
-	my $speciesName = $q->param("speciesName");
-	$fileTags{'organism'} = $speciesName;
-	my $serotype = $q->param("serotype");
-	#serotype does not exist in the database
-	$fileTags{'serotype'} = $serotype;
-	my $inputPrivacy = $q->param("inputPrivacy");
-	#privacy does not exist in the database
-	$fileTags{'privacy'} = $inputPrivacy;
-	if ($inputPrivacy eq "privateUntil"){
-		my $privateUntilDate = $q->param("inputPrivateUntilDate");
-		$fileTags{'privacy'} = $privateUntilDate;
-	}
-	else{
-	}
+    my $buffer;
+    my $FH = $self->_genomeFile->handle();
+    my $bytesread = $FH->read( $buffer, 1024 );
+    while ($bytesread) {
+        $outHandle->print($buffer);
+        $bytesread = $FH->read( $buffer, 1024 );
+    }
 
-	#With the optional fields:
-	#
-	my $description = $q->param("description");
-	$fileTags{'description'} = $description;
-	my $finished = $q->param("finished");
-	$fileTags{'finished'} = $finished;
-	my $keywords = $q->param("keywords");
-	$fileTags{'keywords'} = $keywords;
-	my $molType = $q->param("mol_type");
-	$fileTags{'mol_type'} = $molType;
-	my $owner = $q->param("owner");
-	$fileTags{'owner'} = $owner;
-	my $problem = $q->param("problem");
-	$fileTags{'problem'} = $problem;
-	my $score = $q->param("score");
-	$fileTags{'score'} = $score;
-	my $status = $q->param("status");
-	$fileTags{'status'} = $status;
-	my $symbol = $q->param("symbol");
-	$fileTags{'symbol'} = $symbol;
-
-	#This would likely cause a conflict if two people are uploading a sequence at the same time.
-	#We need to add an id to each file to distinguish one from another.
-
-	my $uploadDir = "../../Sequences/TempUpload/";
-	my $outHandle = IO::File->new('>' . $uploadDir . $genomeFile) or die "$!";
-	
-	my $buffer;
-	my $FH = $genomeFile->handle();
-	my $bytesread = $FH->read( $buffer, 1024 );
-	while ($bytesread) {
-		$outHandle->print($buffer);
-		$bytesread = $FH->read( $buffer, 1024 );
-	}
-	
-	$outHandle->close();
-
-	$self->_uploadToDatabase($genomeFileName , $uploadDir , \%fileTags);
+    $outHandle->close();
+    $self->_processUploadGenome();
+    $self->_aggregateGffs();
+    $self->_uploadToDatabase(dbi => $self->config_param('db.dbi'),
+        dbName => $self->config_param('db.name'),
+        dbHost => $self->config_param('db.host'),
+        dbPort => $self->config_param('db.port'),
+        dbUser => $self->config_param('db.user'),
+        dbPass => $self->config_param('db.pass'));
+    system("rm -r " . $self->_uploadDir) == 0 or die $self->logger->info("System with args failed: $?");
+    return $self->redirect('../strain_info');
 
 }
 
@@ -192,72 +202,33 @@ Helper method to upload genome to database
 
 =cut
 
-sub _uploadToDatabase {
-	my $self  = shift;
-	my $genomeFileName = shift;
-	my $uploadDir = shift;
-	
-	my $logger = Log::Log4perl->get_logger();
+sub _processUploadGenome {
+    my $self=shift;
+    my $fileNumber = 0;
+    my %fileTags = %{$self->_formInputs};
+    my $atts = "";
+    foreach my $key (keys %fileTags) {
+        if (!($fileTags{$key})) {    
+        }
+        else {
+            $atts = $atts . $key ."=" . $fileTags{$key} . ";";
+        }
+    }
+    my $attributes = $atts . "Parent=" . $self->_genomeName;
 
-	#Hash reference to the tags that need to be printed to each fasta file.
-	my $fileTags = shift;
-	my %fileTags = %{$fileTags};
+    system("mkdir -m 7777 " . $self->_uploadDir ."/fastaTemp") == 0 or die $self->logger->info("System with args failed: $?");
+    system("mkdir -m 7777 " . $self->_uploadDir . "/gffsTemp") == 0 or die $self->logger->info("System with args failed: $?");
+    system("mkdir -m 7777 " . $self->_uploadDir . "/gffsToUpload") == 0 or die $self->logger->info("System with args failed: $?");
 
-	my $fileNumber = 0;
+    my $in = Bio::SeqIO->new(-file => $self->_uploadDir . "/" . $self->_genomeFileName, -format => 'fasta');
 
-	my $attributes = "";
-
-	foreach my $key (keys %fileTags) {
-		if (!($fileTags->{$key})) {
-			#$fileTags->{$key} = "";
-		}
-		else {
-			$attributes = $attributes . $key ."=" . $fileTags->{$key} . ";";
-		}
-	}
-
-
-	my $args1 = "mkdir" . " -m 7777 " . "$uploadDir" . "fasta";
-	system($args1) == 0 or die $logger->info("System with $args1 failed: $?");
-	$logger->info("System executed $args1 with value: $?");
-	
-	my $args2 = "mkdir". " -m 7777 " . "$uploadDir" . "gffout" ;
-	system($args2) == 0 or die $logger->info("System with $args2 failed: $?");
-	$logger->info("System executed $args2 with value: $?");
-
-	my $in = Bio::SeqIO->new(-file => "$uploadDir" . "$genomeFileName", -format => 'fasta');
-
-	while (my $seq = $in->next_seq()) {
-		$fileNumber++;
-		my $singleFileName = $seq->id;
-		my $singleFastaHeader = Bio::SeqIO->new(-file => '>' . "$uploadDir/fasta/$singleFileName" . ".fasta" , -format => 'fasta') or die $logger->info("$!");
-		$singleFastaHeader->write_seq($seq) or die $logger->info("$!");
-		$self->_appendAttributes($singleFileName , $uploadDir , $fileNumber , $attributes);
-	}
-
-	my $args3 = "rm -r $uploadDir" . "fasta";
-	system($args3) == 0 or die $logger->info("System with $args3 failed: $?");
-	$logger->info("System executed $args3 with value: $?");
-
-	my $gffOutDir = $uploadDir . 'gffout/';
-	my $gffOutFiles = _getFileNamesFromDirectory($gffOutDir);
-
-	foreach my $gffOutFile(@{$gffOutFiles}) {
-		my $dbArgs = "gmod_bulk_load_gff3.pl --dbname chado_upload_test --dbuser postgres --dbpass postgres --organism \"Escherichia coli\" --gfffile $gffOutDir" . "$gffOutFile" . " --random_tmp_dir";
-		system($dbArgs) == 0 or die $logger->info("System failed with $dbArgs: $?");
-		$logger->info("System executed $dbArgs with value: $?");
-	}
-
-	my $args4 = "rm -r $uploadDir" . "gffout";
-	system($args4) == 0 or die $logger->info("System with $args4 failed: $?");
-	$logger->info("System executed $args4 with value: $?");
-
-	my $args5 = "rm -r $uploadDir" . "$genomeFileName";
-	system($args5) == 0 or die $logger->info("System with $args5 failed: $?");
-	$logger->info("System executed $args5 with value: $?");
-	
-	return $self->redirect('../strain_info');
-
+    while (my $seq = $in->next_seq()) {
+        $fileNumber++;
+        my $singleFileName = $seq->id;
+        my $singleFastaHeader = Bio::SeqIO->new(-file => '>' . $self->_uploadDir . "/fastaTemp/$singleFileName.fasta" , -format => 'fasta') or die $self->logger->info("$!");
+        $singleFastaHeader->write_seq($seq) or die $self->logger->info("$!");
+        $self->_appendAttributes($singleFileName , $fileNumber , $attributes);
+    }
 }
 
 =head2 _appendAttributes
@@ -267,42 +238,111 @@ Tags fasta files with attributes and converts them to .gff files
 =cut
 
 sub _appendAttributes {
-	my $self = shift;
-	my $singleFileName = shift;
-	my $uploadDir = shift;
-	my $fileNumber = shift;
-	my $atts = shift;
-	
-	my $logger = Log::Log4perl->get_logger();
-	
-	$atts =~ s/;$//;
-	my $appendArgs = "gmod_fasta2gff3.pl" . " --attributes " . "\"$atts\"" . " --fasta_dir " . "$uploadDir" . "fasta/" .  " --gfffilename " . "$uploadDir" . "gffout/out" . "$fileNumber" . ".gff";
-	system($appendArgs) == 0 or die $logger->info("System failed with $appendArgs: $?");
-	$logger->info("System executed $appendArgs with value: $?");
-	unlink "$uploadDir" . "fasta/" . "directory.index";
-	unlink "$uploadDir" . "fasta/" . "$singleFileName" . ".fasta";
+    my ($self , $_singleFileName , $_fileNumber , $_attributes) = @_;
+    my $appendArgs = "gmod_fasta2gff3.pl --type contig --attributes \"$_attributes\" --fasta_dir " . $self->_uploadDir . "/fastaTemp/ --gfffilename " . $self->_uploadDir . "/gffsTemp/out$_fileNumber.gff";
+    system($appendArgs) == 0 or die $self->logger->info("System failed with $appendArgs: $?");
+    $self->logger->info("System executed $appendArgs with value: $?");
+    unlink $self->_uploadDir . "/fastaTemp/directory.index";
+    unlink $self->_uploadDir . "/fastaTemp/$_singleFileName.fasta";
 }
 
-=head2 _getFileNamesFromDirectory
+=head2 _aggregateGffs
 
-Opens the specified directory, excludes all filenames beginning with '.' and
-returns the rest as an array ref.
+Aggregates temp gff files into a single file and appends the parent name.
 
 =cut
 
-sub _getFileNamesFromDirectory {
-	my $directory = shift;
+sub _aggregateGffs {
+    my $self = shift;
+    my $gffsTempDir = $self->_uploadDir . "/gffsTemp";
+    opendir (TEMP , $self->_uploadDir . "/gffsTemp") or die "cannot open directory , $!\n";
+    while (my $file = readdir TEMP)
+    {
+        $self->_writeOutFile($file);
+        unlink $self->_uploadDir . "/gffsTemp/$file"; 
+    }
+    $self->_mergeFiles();
+    closedir TEMP;
+}
 
-	opendir( DIRECTORY, $directory ) or die "cannot open directory $directory $!\n";
-	my @dir = readdir DIRECTORY;
-	closedir DIRECTORY;
+=head2 _writeOutFile
 
-	my @fileNames;
-	foreach my $fileName(@dir){
-		next if substr( $fileName, 0, 1 ) eq '.';
-		push @fileNames, ( $fileName );
-	}
-	return \@fileNames;
+Helper function to _aggregateGffs(). Writes out single gff files into a single gff file to be uploaded
+
+=cut
+
+sub _writeOutFile {
+    my ($self, $file) = @_;
+    open my $in , '<' , $self->_uploadDir . "/gffsTemp/$file" or die "Can't write to the file: $!";
+    open my $outTags , '>>' , $self->_uploadDir . "/gffsToUpload/tempTagFile" or die "Can't write to the file: $!";
+    open my $outSeqs , '>>' , $self->_uploadDir . "/gffsToUpload/tempSeqFile" or die "Can't write to the file: $!";
+
+    while (<$in>) {
+        if ($. == 3) {
+            print $outTags $_;
+        }
+        if ($. == 5 || $. == 6){
+            print $outSeqs $_;
+        }
+        else{
+        }
+    }
+    close $outTags;
+    close $outSeqs;
+}
+
+=head2 _mergeFiles
+
+Helper function to _aggregateGffs(). Writes out single gff files into a single gff file to be uploaded
+
+=cut
+
+sub _mergeFiles {
+    my $self = shift;
+    if ($self->_uploadDir . "/gffsToUpload/tempTagFile" && $self->_uploadDir . "/gffsToUploadtempSeqFile") {
+        my $outFileName = "out" . $self->_genomeName . ".gff";
+        open my $inTagFile , '<', $self->_uploadDir . "/gffsToUpload/tempTagFile" or die "Can't read file: $!";
+        open my $inSeqFile , '<', $self->_uploadDir . "/gffsToUpload/tempSeqFile" or die "Can't read file: $!";
+        open my $out , '>>', $self->_uploadDir . "/gffsToUpload/$outFileName";
+
+        print $out $self->_genomeName . "	.	contig_collection	.	.	.	.	.	ID=" . $self->_genomeName . ";Name=" . $self->_genomeName . "\n";
+        while (my $line = <$inTagFile>) {
+            print $out $line;
+        }
+        close $inTagFile;
+        print $out "##FASTA\n";
+        while (my $line = <$inSeqFile>) {
+            print $out $line;
+        }
+        close $inSeqFile;
+        close $out;
+        unlink $self->_uploadDir . "/gffsToUpload/tempTagFile";
+        unlink $self->_uploadDir . "/gffsToUpload/tempSeqFile";
+    }
+    else {
+    }
+}
+
+=head2 _uploadToDataBase
+
+Uploads the aggregated gff file to the database.
+
+=cut
+
+sub _uploadToDatabase {
+    my $self = shift;
+    my %paramsRef = @_;
+    opendir (GFF, $self->_uploadDir . "/gffsToUpload") or die "Can't open directory, $!\n";
+    while (my $gffFile = readdir GFF) {
+        if ($gffFile eq "." || $gffFile eq "..") {
+        }
+        else {
+            my $dbArgs = "gmod_bulk_load_gff3.pl --dbname " . $paramsRef{'dbName'} . " --dbuser " . $paramsRef{'dbUser'} . " --dbPass " . $paramsRef{'dbPass'} . " --organism \"Escherichia coli\" --gfffile " . $self->_uploadDir . "/gffsToUpload/$gffFile --random_tmp_dir";
+            system($dbArgs) == 0 or die "System failed with $dbArgs: $? \n";
+            $self->logger->info("System executed $dbArgs with value: $?");
+        }
+    }
+    closedir GFF;
 }
 
 1;
