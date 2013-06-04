6082acc72b8987bc4ba899cf9e108b5ccd5f12b7
Matt Whiteside <mdwhitesi@gmail.com>
2013-06-04 12:18:06 -0600

    Uploading genomes functionality completed
    Altered GenomeUpdater.pm and added new parsers Adapter.pm and genodo_fasta_loader.pm
    to perform uploads of user submitted genome sequences.
    
    Added new tracking interface to allow users to see the status of the uploaded genome.
    Includes new method "status" and new DB table "tracker".

---
+++ Database/db_dumps/create_tracking_table.sql
@@ -0,0 +1,60 @@
+BEGIN;
+
+--
+-- SQL for creating additional table for tracking progress of analysis of user's
+--   uploaded genome
+--
+
+SET search_path = public, pg_catalog;
+SET default_tablespace = '';
+
+
+-----------------------------------------------------------------------------
+--
+-- Table Name: tracker; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE tracker (
+	tracker_id       integer NOT NULL,
+	step             integer NOT NULL DEFAULT 0,
+	failed           boolean NOT NULL DEFAULT FALSE,
+	feature_name     varchar(255),
+	command          varchar(255),
+	pid              varchar(100),
+	upload_id        integer,
+	login_id         integer NOT NULL DEFAULT 0,
+	start_date       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	end_date         TIMESTAMP
+);
+
+ALTER TABLE public.tracker OWNER TO postgres;
+
+--
+-- primary key
+--
+CREATE SEQUENCE tracker_tracker_id_seq
+	START WITH 1
+	INCREMENT BY 1
+	NO MINVALUE
+	NO MAXVALUE
+	CACHE 1;
+
+ALTER TABLE public.tracker_tracker_id_seq OWNER TO postgres;
+
+ALTER SEQUENCE tracker_tracker_id_seq OWNED BY tracker.tracker_id;
+
+ALTER TABLE ONLY tracker ALTER COLUMN tracker_id SET DEFAULT nextval('tracker_tracker_id_seq'::regclass);
+
+ALTER TABLE ONLY tracker
+	ADD CONSTRAINT tracker_pkey PRIMARY KEY (tracker_id);
+
+--
+-- foreign keys
+--
+ALTER TABLE ONLY tracker
+	ADD CONSTRAINT tracker_login_id_fkey FOREIGN KEY (login_id) REFERENCES login(login_id);
+
+ALTER TABLE ONLY tracker
+	ADD CONSTRAINT tracker_upload_id_fkey FOREIGN KEY (upload_id) REFERENCES upload(upload_id);
+
+COMMIT;
---
+++ Sequences/upload_wrapper.pl
@@ -0,0 +1,216 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use DBI;
+use Getopt::Long;
+use Carp;
+use Config::Simple;
+use POSIX qw(strftime);
+use FindBin;
+use IO::CaptureOutput qw(capture_exec);
+
+=head1 NAME
+
+$0 - Calls multiple scripts for loading and analyzing a user uploaded genome. Notifies user when complete.
+
+=head1 SYNOPSIS
+
+  % $0 [options]
+
+=head1 OPTIONS
+
+ --options-file			file containing the arguments needed for the various scripts 
+
+=head1 AUTHORS
+
+Matthew Whiteside E<lt>matthew.whiteside@phac-aspc.gc.caE<gt>
+
+Copyright (c) 2013
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+# Load arguments
+my ($OPTFILE, $DEBUG, $RECOVER
+);
+
+GetOptions(
+	'optfile=s'=> \$OPTFILE,
+    'debug'   => \$DEBUG,
+    'recover' => \$RECOVER
+);
+
+## Initialization
+
+# Read master config file
+croak "You must supply a options filename" unless $OPTFILE;
+my $opt = new Config::Simple($OPTFILE) or croak "Cannot read config file $OPTFILE (" . Config::Simple->error() .')';
+
+my $tracking_id = $opt->param('main.tracking_id');
+croak "Missing main.tracking_id option in options file." unless $tracking_id;
+
+
+# Connect to Chado DB
+my $dbConfigFile = $opt->param('load.configfile');
+croak "Missing load.configfile option in options file." unless $dbConfigFile;
+
+my ($dbh, $tmp_dir) = connect_db($dbConfigFile);
+
+
+# Tmp files that need to be deleted at end of program
+my @remove_tmp_files = ($opt->param('load.fastafile'), $opt->param('load.propfile'), $OPTFILE);
+
+
+# Common sql statements
+my $sql = qq/UPDATE tracker SET step = ? WHERE tracker_id = $tracking_id/;
+my $update_step_sth = $dbh->prepare($sql);
+
+
+## Loading
+
+# Check for other loading scripts currently running.
+# Only one can run at a time,
+# so wait until no more in queue before running.
+$sql = qq/SELECT count(*) FROM tracker WHERE step = 1 AND failed = FALSE AND tracker_id < $tracking_id/;
+my $sth = $dbh->prepare($sql);
+
+$sth->execute();
+my ($num_ahead) = $sth->fetchrow_array();
+
+while($num_ahead) {
+	sleep(30); # wait 30 sec before trying again
+	
+	$sth->execute();
+	($num_ahead) = $sth->fetchrow_array();
+}
+
+# In front of the line
+
+# Run loading script
+my @loading_args = ("perl $FindBin::Bin/genodo_fasta_loader.pl", '--webupload',
+	"--tracking_id $tracking_id",
+	'--fastafile '.$opt->param('load.fastafile'), 
+	'--configfile '.$opt->param('load.configfile'),
+	'--propfile '.$opt->param('load.propfile'),
+	$opt->param('load.addon_args'));
+	
+push @loading_args, '--remove_lock --recreate_cache' if $RECOVER;
+
+my $cmd = join(' ',@loading_args);
+my ($stdout, $stderr, $success, $exit_code) = capture_exec($cmd);
+
+if($success) {
+	# Update step
+	$update_step_sth->execute(2);
+} else {
+	my $err_msg = "Loading script failed\n $stderr";
+	handle_error($dbh, $tracking_id, $err_msg);
+	exit(1);
+}
+
+
+
+
+
+
+
+## Termination
+
+# Sign off in tracking job
+my $now = strftime "%Y-%m-%d %H:%M:%S", localtime;
+$sql = qq/UPDATE tracker SET end_date = '$now' WHERE tracker_id = $tracking_id/;
+$dbh->do($sql);
+
+# Delete tmp files
+clean_up(@remove_tmp_files);
+
+
+#############
+## Subs
+#############
+
+=head2 connect_db
+
+Establish DBI connection and return
+DBI handle.  Connection parameters are
+passed in INI-style config file.
+
+Also returns tmp
+directory in config file
+
+=cut
+
+sub connect_db {
+	my $dbConfigFile = shift;
+	
+	# Read db connection parameters config file
+	my ($tmp_dir, $DBNAME, $DBUSER, $DBPASS, $DBHOST, $DBPORT, $DBI);
+	if(my $db_conf = new Config::Simple($dbConfigFile)) {
+		$DBNAME    = $db_conf->param('db.name');
+		$DBUSER    = $db_conf->param('db.user');
+		$DBPASS    = $db_conf->param('db.pass');
+		$DBHOST    = $db_conf->param('db.host');
+		$DBPORT    = $db_conf->param('db.port');
+		$DBI       = $db_conf->param('db.dbi');
+		$tmp_dir = $db_conf->param('tmp.dir');
+	} else {
+		die Config::Simple->error();
+	}
+	croak "Missing DB connection parameters in configuration file $dbConfigFile." unless $DBNAME;
+
+	# Connect to DB
+	my $dbh = DBI->connect(
+		"dbi:Pg:dbname=$DBNAME;port=$DBPORT;host=$DBHOST", $DBUSER,$DBPASS,
+		{ AutoCommit => 1, RaiseError => 1 }
+	) or croak "Unable to connect to database";
+	
+	croak "Missing tmp directory in configuration file $dbConfigFile." unless $tmp_dir; 
+	
+	return($dbh, $tmp_dir);
+}
+
+=head2 clean_up
+
+Delete a list of files. Called at
+end to remove tmp files.
+
+=cut
+
+sub clean_up {
+	my @files = @_;
+	
+	foreach my $file (@files) {
+		unlink $file or carp "Error removing ".$file;
+	}
+}
+
+=head2 handle_error
+
+Error occurred. Clean up and update tracker table in DB.
+
+=cut
+
+sub handle_error {
+	my ($dbh, $tracking_id, $err) = @_;
+	
+	# Update tracking table in DB
+	my $sql = qq/UPDATE tracker SET failed = TRUE WHERE tracker_id = $tracking_id/;
+	$dbh->do($sql);
+	
+	# Save error in a tmp file
+	my $err_file = $tmp_dir.'genodo-error-'.$tracking_id.'.txt';
+	
+	$err_file = $opt->param('main.error_file') if $opt && $opt->param('main.error_file');
+	
+	open(ERR, ">$err_file") or croak "Unable to write to file $err_file ($!).\n";
+	print ERR "# Job $tracking_id Error\n# " . localtime .
+			  "\nError Description:\n------------\n$err\n------------\n";
+	close ERR;
+	
+}
+
+
+
