a8be71db372e8698db41ea23252cc48f8e46ec54
amanji <akiff.manji@gmail.com>
2013-07-24 15:55:27 -0600

    Groupwise comparison page now async

--- Modules/Dispatch.pm
+++ Modules/Dispatch.pm
@@ -36,7 +36,7 @@ sub dispatch_args {
                 '/upload_genome' => {app=>'GenomeUploader', rm=>'upload_genome'},
                 '/virulence_factors' =>   {app=>'VirulenceFactors' , rm=>'virulence_factors'},
                 '/statistics' =>    {app=>'Statistics' , rm=>'stats'},
-                '/group_compare_test/' => {app=>'GroupWiseComparisons', rm=>'group_compare_test'},
+                '/group_wise_info/' => {app=>'GroupWiseComparisons', rm=>'group_wise_info'},
                 '/virulence_amr_by_strain/' => {app=>'VirulenceFactors', rm=>'virulence_amr_by_strain'},
                 '/host_source_form' => {app=>'StrainInfo' , rm=>'host_source_form'},
                 '/isolation_source_form' => {app=>'StrainInfo' , rm=>'isolation_source_form'},
