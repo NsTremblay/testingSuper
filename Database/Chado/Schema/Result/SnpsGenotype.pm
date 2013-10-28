4904a1fa68093032823704ee69be8c5ca49f6728
amanji <akiff.manji@gmail.com>
2013-10-28 15:39:43 -0600

    New schemas for master

--- Database/Chado/Schema/Result/SnpsGenotype.pm
+++ Database/Chado/Schema/Result/SnpsGenotype.pm
@@ -30,21 +30,17 @@ __PACKAGE__->table("snps_genotypes");
   is_nullable: 0
   sequence: 'snps_genotypes_snp_genotype_id_seq'
 
-=head2 feature_id
+=head2 genome_id
 
   data_type: 'text'
   is_nullable: 0
   original: {data_type => "varchar"}
 
-Strain ID
-
-=head2 snp_id
+=head2 feature_id
 
   data_type: 'integer'
   is_foreign_key: 1
   is_nullable: 0
-
-Stores a snp ID from the snps table
 
 =head2 snp_a
 
@@ -88,13 +84,13 @@ __PACKAGE__->add_columns(
     is_nullable       => 0,
     sequence          => "snps_genotypes_snp_genotype_id_seq",
   },
-  "feature_id",
+  "genome_id",
   {
     data_type   => "text",
     is_nullable => 0,
     original    => { data_type => "varchar" },
   },
-  "snp_id",
+  "feature_id",
   { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
   "snp_a",
   { data_type => "integer", default_value => 0, is_nullable => 0 },
@@ -120,24 +116,24 @@ __PACKAGE__->set_primary_key("snp_genotype_id");
 
 =head1 RELATIONS
 
-=head2 snp
+=head2 feature
 
 Type: belongs_to
 
-Related object: L<Database::Chado::Schema::Result::Snp>
+Related object: L<Database::Chado::Schema::Result::Feature>
 
 =cut
 
 __PACKAGE__->belongs_to(
-  "snp",
-  "Database::Chado::Schema::Result::Snp",
-  { snp_id => "snp_id" },
+  "feature",
+  "Database::Chado::Schema::Result::Feature",
+  { feature_id => "feature_id" },
   { is_deferrable => 1, on_delete => "CASCADE,", on_update => "NO ACTION" },
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07035 @ 2013-09-10 14:32:56
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Xmmhe1PoLNaBVBjY0WXE6A
+# Created by DBIx::Class::Schema::Loader v0.07035 @ 2013-10-28 15:38:00
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:3Qg+o7+FBVNTtHaDgdoy1w
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
