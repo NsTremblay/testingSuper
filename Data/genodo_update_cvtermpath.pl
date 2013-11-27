4ef959399643940829f754bc050090dedd251a8f
amanji <akiff.manji@gmail.com>
2013-11-26 17:17:10 -0700

    ...

--- Data/genodo_update_cvtermpath.pl
+++ Data/genodo_update_cvtermpath.pl
@@ -67,25 +67,105 @@ my $cvterm = $schema->resultset('Cvterm')->find({'me.name' => 'antimicrobial_res
 my $id = $cvterm->cvterm_id;
 die unless $id;
 
-my $geneResults = $schema->resultset('FeatureCvterm')->search(
-	{'feature.type_id' => $id},
+# my $geneResults = $schema->resultset('FeatureCvterm')->search(
+# 	{'feature.type_id' => $id},
+# 	{
+# 		#join => [{'cvterm' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => 'object'}}}}}}}}}}}}}}, 'feature'],
+# 		#select => ['me.feature_id','cvterm.name', 'me.cvterm_id', 'cvterm.name' , 'cvterm_relationship_subjects.object_id', 'object_7.name'],
+# 		#as => ['gene_id', 'type_name', 'gene_cvterm_id', 'gene_cvterm_name', 'cvterm_parent_id', 'cvterm_parent_name']
+# 	}
+# 	);
+
+#Categories will have the form:
+# %categores = (
+# 	'unclassified' => {
+# 		{ 
+# 			type_id => [feature_id..], here the type_id is the cvterm_id of the gene.
+# 		}
+# 	},
+#
+### Otherwise the has should have the form:
+#
+#	'parent_name' => {
+#		parent_id => '###',
+#			'subcategories' => {
+#				cvterm_name => {
+#				cvterm_id => '###',
+#				genes => {type_id = []}
+#			}
+#		}
+#	}
+# )
+
+
+my %unclassifiedIds;
+
+#Bottom -> Up:
+my $amrGeneResutls = $schema->resultset('FeatureCvterm')->search(
+	{},
 	{
-		join => [{'cvterm' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => {'object' => {'cvterm_relationship_subjects' => 'object'}}}}}}}}}}}}}}, 'feature'],
-		#select => ['me.feature_id','cvterm.name', 'me.cvterm_id', 'cvterm.name' , 'cvterm_relationship_subjects.object_id', 'object_7.name'],
-		#as => ['gene_id', 'type_name', 'gene_cvterm_id', 'gene_cvterm_name', 'cvterm_parent_id', 'cvterm_parent_name']
+
+		select => ['me.feature_id', 'me.cvterm_id'],
+		as => ['feature_id', 'type_id']
 	}
 	);
 
-my %ontologyTree = $geneResults->all;
-# while (my $row = $geneResults->next) {
-# 	$ontologyTree{$row->get_column('cvterm_parent_name')} = [] unless exists $ontologyTree{$row->get_column('cvterm_parent_name')};
-# 	#$ontologyTree{$row->get_column('gene_cvterm_name')} = [] unless exists $ontologyTree{$row->get_column('gene_cvterm_name')};
-# 	#push(@{$ontologyTree{$row->get_column('gene_cvterm_name')}}, $row->get_column('gene_id'));
+my %categories;
+$categories{'unclassified'} = {};
+
+
+while (my $row = $amrGeneResutls->next) {
+	$categories{'unclassified'}->{$row->get_column('type_id')} = [] unless exists $categories{'unclassified'}->{$row->get_column('type_id')};
+	push(@{$categories{'unclassified'}->{$row->get_column('type_id')}}, $row->get_column('feature_id'));
+	$unclassifiedIds{$row->get_column('type_id')} = undef;
+}
+
+#while (keys {%$categories{'unclassified'}}) != 0) {
+# 	##Find the parent
 # }
 
-foreach my $keys (keys %ontologyTree) {
-	foreach my $subkeys (keys %{$ontologyTree{$keys}}) {
-		print $subkeys . "\n";
+sub FindParent {
+	##Key is to only remove the key if the parent_id is "process or component of antibiotic biology or chemistry"
+	my $unclassifiedIds = shift;
+}
+
+my @wantedCategories = (
+	'antibiotic molecule',
+	'determinant of antibiotic resistance',
+	'antibiotic target'
+	);
+
+my %categoryIds;
+getCategoryIds(\@wantedCategories);
+
+sub getCategoryIds {
+	my $wantedCategories = shift;
+	my $categoryResults = $schema->resultset('Cvterm')->search(
+		{'dbxref.accession' => '1000001', 'subject.name' => $wantedCategories},
+		{
+			join => [
+			'dbxref',
+			{'cvterm_relationship_objects' => {'subject' => [{'cvterm_relationship_objects' => 'subject'}, 'dbxref']}}
+			],
+			select => ['me.dbxref_id', 'subject.cvterm_id', 'subject.name', 'subject_2.cvterm_id', 'subject_2.name', 'dbxref_2.accession'],
+			as => ['parent_dbxref_id', 'broad_category_id', 'broad_category_name', 'refined_category_id', 'refined_category_name', 'accession']
+		}
+		);
+
+	while (my $row = $categoryResults->next) {
+		my %subcategory;
+		$subcategory{'cvterm_id'} = $row->get_column('refined_category_id');
+		$subcategory{'genes'} = {};
+		$categories{$row->get_column('broad_category_name')}->{'subcategories'} = {} unless exists $categories{$row->get_column('broad_category_name')}->{'subcategories'};
+		$categories{$row->get_column('broad_category_name')}->{'subcategories'}->{$row->get_column('refined_category_name')} = \%subcategory;
+		$categories{$row->get_column('broad_category_name')}->{'parent_id'} = $row->get_column('broad_category_id');
+		$categoryIds{$row->get_column('refined_category_id')} = undef;
 	}
 }
-print scalar(keys %ontologyTree) . "\n";
+
+foreach my $x (keys %categories) {
+	print "$x: \n" . join("\t", keys %{$categories{$x}}) . "\n\n";
+}
+
+print scalar(keys %unclassifiedIds) . "\n";
+print scalar(keys %categoryIds) . "\n";
