78082eba366b0a5570aba2425775a4030fdc5d32
Chad Laing <chadlaing@gmail.com>
2014-10-17 14:30:33 -0600

    Update for correct printing of output

--- Sequences/retrieve_factor_nucleotide_sequences.pl
+++ Sequences/retrieve_factor_nucleotide_sequences.pl
@@ -11,6 +11,7 @@ my $inputFile = $ARGV[0];
 my $inFH = IO::File->new('<' . $inputFile) or die "$!";
 
 my %vf;
+my @searchKeys;
 
 while(my $line = $inFH->getline()){
     unless($line =~ m/^>/){
@@ -21,6 +22,7 @@ while(my $line = $inFH->getline()){
     my @la = split(/\s+/, $line);
 
     if($la[0] =~ m/>(.+)/){
+        push @searchKeys, $la[1];
         my $geneName = $1;
         $vf{$la[1]}->{geneName}=$geneName;
 
@@ -42,21 +44,28 @@ while(my $line = $inFH->getline()){
         print "Could not find gene name\n";
         exit(1);
     }
+    last;
 }
-
 
 
 my $query = Bio::DB::Query::GenBank->new(
         -db=>'nucleotide',
-        -ids=>['U12894']
+        -ids=>\@searchKeys
     );
 
 
 my $gb = Bio::DB::GenBank->new();
 my $stream = $gb->get_Stream_by_query($query);
 
+my $counter=0;
 while(my $seq = $stream->next_seq()){
-    #print('>' . $seq->id() . ' ' . $seq->desc() . "\n" . $seq->seq() . "\n");
+    print('>' . $vf{$searchKeys[$counter]}->{geneName}  . ' ' . $seq->desc() .
+        ' [' . $searchKeys[$counter] . ' ' . $vf{$searchKeys[$counter]}->{start} . '-' . 
+        $vf{$searchKeys[$counter]}->{stop} . "]\n" . 
+        $seq->subseq($vf{$searchKeys[$counter]}->{start},$vf{$searchKeys[$counter]}->{stop}) . "\n");
+}
+continue{
+    $counter++;
 }
 
 
