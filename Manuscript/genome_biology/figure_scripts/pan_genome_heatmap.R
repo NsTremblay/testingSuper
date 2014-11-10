782b811e6a3ff86eb7366c3aaeb2eaf0c84cfef4
Chad Laing <chadlaing@gmail.com>
2014-11-10 15:07:40 -0700

    Added tree reading and ordering

--- Manuscript/genome_biology/figure_scripts/pan_genome_heatmap.R
+++ Manuscript/genome_biology/figure_scripts/pan_genome_heatmap.R
@@ -7,14 +7,15 @@ library(ggdendro)
 library(RColorBrewer)
 library(argparser)
 library(reshape2)
+library(ape)
 
 
 parser <- arg.parser("Create a heatmap of the pan-genome distribution")
 parser <- add.argument(parser
-  ,c("input")
-  ,help = c("Tab-delimited file of pan-genome presence / absence data")
-  ,short = c("-i")
-  ,default = list(""))
+  ,c("input","tree")
+  ,help = c("Tab-delimited file of pan-genome presence / absence data", "Tree of same data as the table file, with identical names.")
+  ,short = c("-i","-t")
+  ,default = list("",""))
 
 #get all the parameters into usable format
 argv <- parse.args(parser)
@@ -23,6 +24,14 @@ argv <- parse.args(parser)
 #binary data indicating the presence / absence of a segment based on the Panseq
 #run settings
 binaryData <- read.table(file=argv$input, header=TRUE,sep="\t",check.names=TRUE,row.names=1);
+
+
+#tree of the same data, with the same names on the leaves as column headers for the table
+newickTree <- read.tree(file=argv$tree)
+
+#order of the genomes in the tree
+orderOfTree <-newickTree$tip.label 
+orderOfTree
 
 rNames <- rownames(binaryData)
 cNames <- colnames(binaryData)
@@ -37,7 +46,7 @@ mData <- melt(binaryData,id.vars=c("id"), value.name="value", variable.name="var
 #create a mapping for values and colours (this will allow the legend to use these)
 #values, rather than a continuous gradient
 
-heatmap <- ggplot(mData, aes(x=variable, y=id)) + geom_raster(aes(fill=value, width=5)) + scale_fill_continuous(low="white",high="black", breaks=c(0,1), guide="legend") + scale_x_discrete(expand=c(0,0),labels=c("")) + scale_y_discrete(expand=c(0,0),labels=c(""))
+heatmap <- ggplot(mData, aes(x=variable, y=id)) + geom_raster(size=10, hpad=0, vpad=0, aes(fill=value, width=5)) + scale_fill_continuous(low="white",high="black", breaks=c(0,1), guide="legend") + scale_x_discrete(expand=c(0,0),labels=c("")) + scale_y_discrete(expand=c(0,0),labels=c("")) + coord_equal()
 
 #using the gtable package (http://cran.r-project.org/web/packages/gtable/index.html)
 #add a row to the heatmap for the dendrogram
